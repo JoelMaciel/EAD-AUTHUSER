6a561572edb23665ffc87cea87d291a8117e6ed2
Joel Maciel <soldado25461@gmail.com>
2022-11-30 11:31:32 -0300

    feat: implementing user post with partial name and email validations.

--- src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -30,6 +30,21 @@ public class UserServiceImpl implements UserService {
     public void delete(UserModel userModel) {
         userRepository.delete(userModel);
     }
+
+    @Override
+    public UserModel save(UserModel userModel) {
+        return userRepository.save(userModel);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
 
 
