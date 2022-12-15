20cd3f8460cd0f7a09ddaee368ee897466e46ee8
Joel Maciel <soldado25461@gmail.com>
2022-12-15 15:27:50 -0300

    feat: Preparing the getAll methods of course and authuser for searching userCourse and courseUser and implementing searches with criteria builder in the SpecificationTemplate of Course and Authuser .

--- src/main/java/com/ead/authuser/controllers/UserController.java
+++ src/main/java/com/ead/authuser/controllers/UserController.java
@@ -37,17 +37,23 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<Page<UserModel>> getAllUsers(SpecificationTemplate.UserSpec spec,
-    @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable){
+    @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
+                                                       @RequestParam(required = false) UUID courseId){
 
-        Page<UserModel> userModelPage = userService.findAll(spec ,pageable);
-
+        Page<UserModel> userModelPage = null;
+        if( courseId != null) {
+            userModelPage = userService.findAll(SpecificationTemplate.userCourseId(courseId).and(spec), pageable);
+            for(UserModel user : userModelPage.toList()) {
+                user.add(linkTo(methodOn(UserController.class).getOneUser(user.getUserId())).withSelfRel());
+            }
+        } else {
+            userModelPage = userService.findAll(spec ,pageable);
+        }
         if(!userModelPage.isEmpty()) {
             for(UserModel user : userModelPage.toList()) {
                 user.add(linkTo(methodOn(UserController.class).getOneUser(user.getUserId())).withSelfRel());
             }
         }
-
-        var users = userService.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(userModelPage);
     }
 
