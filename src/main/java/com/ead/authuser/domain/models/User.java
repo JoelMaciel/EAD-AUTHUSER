34db3b444b0d672df301281b54a1aa94a4ad8680
Joel <joel.viana@audora.com.br>
2023-09-24 16:33:08 -0300

    feat: create migration and entity of user

---
+++ src/main/java/com/ead/authuser/domain/models/User.java
@@ -0,0 +1,63 @@
+package com.ead.authuser.domain.models;
+
+import com.ead.authuser.domain.enums.UserStatus;
+import com.ead.authuser.domain.enums.UserType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private UserStatus userStatus;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @Column
+    private String phoneNumber;
+
+    @CPF
+    @Column
+    private String cpf;
+
+    @Column
+    @CreationTimestamp
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+}
